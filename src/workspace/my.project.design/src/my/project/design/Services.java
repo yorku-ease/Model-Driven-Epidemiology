a17d22b09c8fe8a02e88f668cef62fc5195f477b
BrunoC-L <bruno.bcl@sympatico.ca>
2022-08-18 03:37:03 -0400

    Added creation of representations.aird file like sirius does and added epimodel file to the representations

--- src/workspace/my.project.design/src/my/project/design/Services.java
+++ src/workspace/my.project.design/src/my/project/design/Services.java
@@ -2,9 +2,13 @@ package my.project.design;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.eclipse.emf.ecore.EObject;
 
@@ -20,21 +24,27 @@ public class Services {
         return (List<EObject>) objclass.getMethod(methodName).invoke(self);
     }
     
-//    public Collection<EObject> childrenOfChildren(EObject self) {
-//    	return self.eContents().stream().map(EObject::eContents).flatMap(List::stream).collect(Collectors.toList());
-//    }
+    public Collection<EObject> childrenOfChildrenAndChildrenOfChildrenOfChildren(EObject self) {
+    	Stream<EObject> l1 = self
+    			.eContents()
+    			.stream()
+    			.map(EObject::eContents)
+    			.flatMap(List::stream);
+    	Stream<EObject> l2 = self
+    			.eContents()
+    			.stream()
+    			.map(EObject::eContents)
+    			.flatMap(List::stream)
+    			.map(EObject::eContents)
+    			.flatMap(List::stream);
+    	return Stream.concat(l1, l2).collect(Collectors.toList());
+    }
     
+    // https://www.eclipse.org/forums/index.php/m/1854385/#msg_1854385
 	public String flowLabel(EObject self, Object target) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
     	target = invoke(target, "getTargetNode");
     	target = invoke(target, "getTarget");
         return (String) self.getClass().getMethod("getTargetRelation", EObject.class).invoke(self, target);
-    }
-    
-	public String flowLabel2(EObject self, Object target) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-		// view.oclAsType(DEdge).targetNode.oclAsType(DSemanticDecorator).target.toString()
-    	Class<?> objclass = target.getClass();
-    	Method[] ms = objclass.getMethods();
-    	return Arrays.asList(ms).stream().map(m->m.getName()).collect(Collectors.joining(", "));
     }
 	
 	Object invoke(Object obj, String method, Object... args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
--- src/workspace/org.epimodel/src/org/epimodel/CustomProjectSupport.java
+++ src/workspace/org.epimodel/src/org/epimodel/CustomProjectSupport.java
@@ -2,12 +2,17 @@ package org.epimodel;
 
 import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
+import org.eclipse.core.commands.Command;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -17,47 +22,67 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.impl.EPackageRegistryImpl;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceFactoryRegistryImpl;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.swt.SWT;
 import org.epimodel.natures.EpimodelProjectNature;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.tools.api.command.semantic.AddSemanticResourceCommand;
+import org.eclipse.sirius.ui.tools.api.project.ModelingProjectManager;
 
-
-/*
- * Copied from:
- * 
- * - Writing an Eclipse Plug-in (Part 4): Create a Custom Project in Eclipse – New Project Wizard: the Behavior
- * - July 26, 2009, cvalcarcel
- * - https://cvalcarcel.wordpress.com/2009/07/26/writing-an-eclipse-plug-in-part-4-create-a-custom-project-in-eclipse-new-project-wizard-the-behavior/
- * 
- */
-
+import epimodel.impl.EpimodelFactoryImpl;
 
 public class CustomProjectSupport {
     
     public static IProject createProject(
 		String projectName,
-		URI location,
+		java.net.URI location,
 		List<String> availableExtensions,
 		List<Boolean> extensionTruthValues
     ) {
         Assert.isNotNull(projectName);
         Assert.isTrue(projectName.trim().length() > 0);
- 
         IProject project = createBaseProject(projectName, location);
         try {
-            addNature(project);
+            addNature(project, "org.eclipse.sirius.nature.modelingproject");
+            addNature(project, EpimodelProjectNature.NATURE_ID);
 
-            String[] folder_paths = { "parent/child1-1/child2", "parent/child1-2/child2/child3" };
-            String[] file_paths = { "parent/child1-1/child2/test.xml", "parent/child1-2/child2/child3/file.txt", "extensions.txt" };
+//            List<String> folder_paths = Arrays.asList("modeling");
+//            List<String> file_paths = Arrays.asList("modeling/model.epimodel", "extensions.txt");
+            List<String> folder_paths = Arrays.asList();
+            List<String> file_paths = Arrays.asList("extensions.txt");
             List<IFile> files = addToProjectStructure(project, folder_paths, file_paths);
-            
-            files.get(2);
 
 			for (int i = 0; i < availableExtensions.size(); ++i)
-				files.get(2).appendContents(
+				files.get(file_paths.indexOf("extensions.txt")).appendContents(
 						new ByteArrayInputStream((availableExtensions.get(i) + ": " + extensionTruthValues.get(i) + "\n").getBytes()),
 						SWT.NONE,
 						null);
-            
+			
+			String model_fn = projectName + ".epimodel";
+			String model_fn_path = project.getFile(model_fn).getLocationURI().toString().substring(6);
+			
+			createEpimodel(model_fn_path);
+			
+			Session newSession = ModelingProjectManager.INSTANCE.createLocalRepresentationsFile(project, new NullProgressMonitor());
+			AddSemanticResourceCommand addSemCommand = new AddSemanticResourceCommand(newSession, URI.createFileURI(model_fn_path), new NullProgressMonitor());
+			newSession.getTransactionalEditingDomain().getCommandStack().execute(addSemCommand);
+			
+//			new AddSemanticResourceCommand(newSession, null, null);
+//			Set<ISessionFileLoadingListener> sessionFileLoadingListeners = SiriusEditPlugin.getPlugin().getSessionFileLoadingListeners();
+//            for (ISessionFileLoadingListener sessionFileLoadingListener : sessionFileLoadingListeners) {
+//                sessionFileLoadingListener.notifySessionLoadedFromModelingProject(newSession);
+//            }
+//			files.get(file_paths.indexOf("representations.aird")).appendContents(new ByteArrayInputStream("".getBytes()), SWT.NONE, null);
+
+			
             try {
     			PrintWriter writer = new PrintWriter("extensions.txt", "UTF-8");
     			writer.close();
@@ -66,7 +91,7 @@ public class CustomProjectSupport {
                 project = null;
     		}
             
-        } catch (CoreException e) {
+        } catch (CoreException | IOException | IllegalArgumentException | SecurityException e) {
             e.printStackTrace();
             project = null;
         }
@@ -74,34 +99,32 @@ public class CustomProjectSupport {
         return project;
     }
  
-    private static IProject createBaseProject(String projectName, URI location) {
-        // it is acceptable to use the ResourcesPlugin class
+    private static IProject createBaseProject(String projectName, java.net.URI location) {
         IProject newProject = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
- 
-        if (!newProject.exists()) {
-            URI projectLocation = location;
-            IProjectDescription desc = newProject.getWorkspace().newProjectDescription(newProject.getName());
-            if (location != null && ResourcesPlugin.getWorkspace().getRoot().getLocationURI().equals(location))
-                projectLocation = null;
- 
-            desc.setLocationURI(projectLocation);
-            try {
-                newProject.create(desc, null);
-                if (!newProject.isOpen()) {
-                    newProject.open(null);
-                }
-            } catch (CoreException e) {
-                e.printStackTrace();
-            }
+        if (newProject.exists())
+        	return newProject;
+        
+        IProjectDescription desc = newProject.getWorkspace().newProjectDescription(newProject.getName());
+        if (location != null && ResourcesPlugin.getWorkspace().getRoot().getLocationURI().equals(location))
+            desc.setLocationURI(null);
+        else
+        	desc.setLocationURI(location);
+        
+        try {
+            newProject.create(desc, null);
+            if (!newProject.isOpen())
+                newProject.open(null);
+        } catch (CoreException e) {
+            e.printStackTrace();
         }
- 
         return newProject;
     }
  
-    private static List<IFile> addToProjectStructure(IProject newProject, String[] folders, String[] files) throws CoreException {
+    private static List<IFile> addToProjectStructure(IProject newProject, List<String> folders, List<String> files) throws CoreException {
         for (String path : folders)
             createFolder(newProject.getFolder(path));
-        List<IFile> l = new ArrayList<>(files.length);
+        
+        List<IFile> l = new ArrayList<>(files.size());
         for (String path : files) {
         	IFile f = newProject.getFile(path);
         	createFile(f);
@@ -125,17 +148,54 @@ public class CustomProjectSupport {
     		null);
     }
  
-    private static void addNature(IProject project) throws CoreException {
-    	if (project.hasNature(EpimodelProjectNature.NATURE_ID))
+	private static void addNature(IProject project, String nature) throws CoreException {
+    	if (project.hasNature(nature))
     		return;
+    	
         IProjectDescription description = project.getDescription();
         {
             String[] prevNatures = description.getNatureIds();
             String[] newNatures = new String[prevNatures.length + 1];
             System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
-            newNatures[prevNatures.length] = EpimodelProjectNature.NATURE_ID;
+            newNatures[prevNatures.length] = nature;
             description.setNatureIds(newNatures);
         }
         project.setDescription(description, null);
     }
+	
+//	private static void printNatures(String projectName) {
+//        IProject newProject = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+//        if (newProject.exists()) {
+//        	try {
+//        		for (String nature : newProject.getDescription().getNatureIds())
+//        			System.out.println(nature);
+//			} catch (CoreException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//        }
+//	}
+	
+	private static void createEpimodel(String model_fn) throws IOException {
+		Resource.Factory.Registry factoryRegistry = new ResourceFactoryRegistryImpl(); {
+			factoryRegistry.getExtensionToFactoryMap().put("*", new EcoreResourceFactoryImpl());
+		}
+		
+        EPackage.Registry pkgRegistry = new EPackageRegistryImpl(); {
+	        pkgRegistry.put(epimodel.EpimodelPackage.eNS_URI, epimodel.EpimodelPackage.eINSTANCE);
+		}
+        
+        ResourceSet resSet = new ResourceSetImpl(); {
+	        resSet.setPackageRegistry(pkgRegistry);
+	        resSet.setResourceFactoryRegistry(factoryRegistry);
+        }
+        
+		{
+			org.eclipse.emf.common.util.URI uri = org.eclipse.emf.common.util.URI.createFileURI(model_fn);
+	        Resource resource = resSet.createResource(uri);
+	        resource.getContents().add(EpimodelFactoryImpl.eINSTANCE.createEpidemicWrapper());
+	        resource.save(null);
+			System.out.println(uri.toFileString());
+		}
+	}
 }
