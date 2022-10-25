855c6b8021a4b7531d5f6f78b1eafb29f4fa4fc8
BrunoC-L <bruno.bcl@sympatico.ca>
2022-10-25 14:51:31 -0400

    temp

--- src/workspace/my.project.design/src/my/project/design/Services.java
+++ src/workspace/my.project.design/src/my/project/design/Services.java
@@ -12,9 +12,9 @@ public class Services {
         return flow.getTargetObjects();
     }
     
-    // to understand services that require parameters
-    // https://www.eclipse.org/forums/index.php/m/1854385/#msg_1854385
+    // to understand services that require parameters: https://www.eclipse.org/forums/index.php/m/1854385/#msg_1854385
 	public String flowLabel(Flow flow, Object target) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		// target is a dnode something which getTarget points to the eobject compartment
     	return flow.getTargetRelation((EObject) target.getClass().getMethod("getTarget").invoke(target));
     }
 }
