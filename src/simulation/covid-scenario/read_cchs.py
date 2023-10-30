82cfb0e1d72372b77766346d6c79835f1fc827a7
Bruno Curzi-Laliberte <bruno.bcl@sympatico.ca>
2023-10-30 18:24:35 -0400

    fixed couting

--- src/simulation/covid-scenario/read_cchs.py
+++ src/simulation/covid-scenario/read_cchs.py
@@ -10,7 +10,7 @@ n_age_columns = len(ages)
 # do not distribute cchs1.csv, share privately within the team if required
 # cchs1 is built aggregating the aforementioned comorbidities and age groups
 # the file is built using beyond 20/20 and exporting to csv, no modification needed
-csv = CSV(open('cchs1.csv').readlines(), ',')
+csv = CSV(open('C:/Users/Bruno/Desktop/cchs1.csv').readlines(), ',')
 
 # this script takes the file as input and generates a file that can be publicly shared
 # the generated file is required for our epidemiological model of Covid-19
@@ -27,16 +27,7 @@ print(len(csv.headers), len(csv.headers) == n_age_columns + n_comorbidity_column
 # order of the comorbidities does not matter
 print(csv.headers) # ['CCC_101', 'CCC_151', 'CCC_031', 'CCC_071', 'CCC_121', 'CCC_131', 'Total', '12 TO 14 YEARS', '15 TO 17 YEARS', '18 TO 19 YEARS', '20 TO 24 YEARS', '25 TO 29 YEARS', '30 TO 34 YEARS', '35 TO 39 YEARS', '40 TO 44 YEARS', '45 TO 49 YEARS', '50 TO 54 YEARS', '55 TO 59 YEARS', '60 TO 64 YEARS', '65 TO 69 YEARS', '70 TO 74 YEARS', '75 TO 79 YEARS', '80 YEARS OR OLDER']
 
-def add_if_comorbidity(row, start_at, into):
-    if row[start_at] == 'YES':
-        for element in row[start_at + 1:n_comorbidity_columns]:
-            if element == 'YES' or element == 'Total':
-                return
-        for i, val in enumerate(row[-n_age_columns:]):
-            if val != '-' and val != '-\n':
-                into[i] += int(val)
-
-def add2(row, into):
+def add_if_comorbidity(row, into):
     has_yes = False
     for element in row[:n_comorbidity_columns]:
         if element == 'YES':
@@ -48,12 +39,9 @@ def add2(row, into):
             if val != '-' and val != '-\n':
                 into[i] += int(val)
 
-csv.data  = csv.data
 vals = [0] * n_age_columns
 for row in csv.data:
-#    for i in range(n_comorbidity_columns):
-    # add_if_comorbidity(row, 0, vals)
-    add2(row, vals)
+    add_if_comorbidity(row, vals)
 
-print(sum(vals))
-print(vals)
+print(sum(vals)) # 8786597
+print(vals) # [119742, 133823, 102439, 299146, 300950, 262045, 336907, 458766, 571581, 844088, 997026, 1049674, 1025910, 835051, 648777, 800672]
