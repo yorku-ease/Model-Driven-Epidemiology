b7a31c94aaded0b8eed5ef84354814d6abce4b3f
Bruno Curzi-Laliberte <bruno.bcl@sympatico.ca>
2023-10-31 16:31:15 -0400

    added pythoncode to covid sim to fix all data to end with 75 years or older compartment

--- src/simulation/covid-scenario/covid_scenario.py
+++ src/simulation/covid-scenario/covid_scenario.py
@@ -1,45 +1,65 @@
 from parameters_setting import *
+from mycsv import CSV
+import read_cchs
 
-def transform(point, transforms):
-    if transforms == []:
-        return point
-    else:
-        return [f(p) for f, p in zip(transforms, point)]
-
-class CSV:
-    def __init__(self, lines, delim, transforms=[]):
-        self.headers = list(map(lambda header: header.strip(), lines[0].split(delim)))
-        self.data = list(map(lambda line: transform(line.split(delim), transforms), lines[1:]))
-    
-    def get(self, *cols):
-        indices = [i for i, col in enumerate(self.headers) if col in cols]
-        for point in self.data:
-            yield [point[i] for i in indices]
-    
-    def get_simple(self, col):
-        for point in self.get(col):
-            yield point[0]
+# parameterization of the model following https://www.cmaj.ca/content/cmaj/suppl/2020/04/08/cmaj.200476.DC1/200476-res-1-at.pdf
 
 def can_gov_ages_to_range(ages):
-    if 'over' in ages:
+    if 'over' in ages or 'older' in ages:
         return [int(ages.split(' ')[0]), 0]
     else:
         ages = ages.split('years')[0].split(' to ')
         return [int(ages[0]), int(ages[1])]
 
-def can_gov_count_to_int(count):
-    return int(count.strip().replace(',', ''))
+contact_last_age_delim = 70
+pop_last_age_delim = 100
 
-polymod_last_age_delim = 70
-n_age_grouped_by_5 = int(polymod_last_age_delim / 5)
+# https://www150.statcan.gc.ca/n1/en/pub/91-215-x/91-215-x2019001-eng.pdf?st=QscDWW2B
+pop_csv = CSV('pop-2019.csv', ',', [can_gov_ages_to_range, int])
 
-can_gov_csv = CSV(open('pop.csv').readlines(), '\t', [can_gov_ages_to_range, can_gov_count_to_int])
-can_gov_csv.data = can_gov_csv.data[:n_age_grouped_by_5] +\
-    [[[polymod_last_age_delim, 0], sum(list(can_gov_csv.get_simple('count'))[n_age_grouped_by_5:])]]
+ # https://www150.statcan.gc.ca/n1/en/catalogue/82M0013X2016001
+comorbidity_csv = CSV('cchs-out.csv', ',', [can_gov_ages_to_range, float]) 
 
-contact_csv = CSV(open('polymod/matrix.csv').readlines(), ',', [float] * (n_age_grouped_by_5 + 1)) # https://github.com/epiforecasts/socialmixr/issues/1
-TOTAL_POP = sum(map(int, can_gov_csv.get_simple('count')))
-# https://www.cmaj.ca/content/cmaj/suppl/2020/04/08/cmaj.200476.DC1/200476-res-1-at.pdf
+# https://github.com/epiforecasts/socialmixr/issues/1
+contact_csv = CSV('polymod_matrix.csv', ',', [float] * (int(contact_last_age_delim / 5) + 1)) 
+
+# now we need to reshape the data into the same groups used by the researchers
+# The last age group (i = 16) included the population aged 75 and older
+
+# first lets do the contacts matrix
+# we need to:
+#   1. rename 70+ to 70-74
+contact_csv.headers[-1] = [70, 74]
+#   2. add column 75+
+contact_csv.headers += [[75, 0]]
+for row in contact_csv.data:
+    row += [0]
+#   3. add row 75+, duplicate the contacts from 70+ to 75+ (we will simply assume they follow the same contact pattern)
+contact_csv.data += [list(contact_csv.data[-1])]
+#   5. divide contacts into the last column using the correct population ratio
+counts_70_plus = list(pop_csv.get_simple('count'))[-7:]
+p_70_75_given_over_70 = counts_70_plus[0] / sum(counts_70_plus)
+for row in contact_csv.data:
+    row[-2]
+    row[-1]
+    row[-2], row[-1] = (p_70_75_given_over_70 * row[-2], (1 - p_70_75_given_over_70) * row[-2])
+
+# then we will do the comorbidity table
+# p(comorbidity given 75+) = (p(comorbidity given 80+) * p(80+) + p(comorbidity given 75-80) * p(75-80))/p(75+)
+p_comorbidity_given_80_plus = comorbidity_csv.data[-1][1]
+p_comorbidity_given_75_80 = comorbidity_csv.data[-2][1]
+# we will use absolute values instead of probabilities for population of a certain age, as it cancels out
+p_80_plus = sum(list(pop_csv.get_simple('count'))[-6:])
+p_75_80 = list(pop_csv.get_simple('count'))[-6]
+p_comorbidity_given_75_plus = (p_comorbidity_given_80_plus * p_80_plus + p_comorbidity_given_75_80 * p_75_80)/(p_75_80 + p_80_plus)
+# change 75-79 to 75+, set new value, remove 80+
+comorbidity_csv.data = comorbidity_csv.data[:-2] + [[75, 0], p_comorbidity_given_75_plus]
+
+# finally we will do the population table
+count_75_plus = sum(list(pop_csv.get_simple('count'))[-6:])
+pop_csv.data = pop_csv.data[:-6]
+pop_csv.data += [[75, 0], count_75_plus]
+
 
 def main():
     project_name = 'covid'
@@ -62,5 +82,4 @@ def main():
     save_parameters(folder + project_name, parameters)
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__": main()
---
+++ src/simulation/covid-scenario/mycsv.py
@@ -0,0 +1,28 @@
+def transform(point, transforms):
+    if transforms == []:
+        return point
+    else:
+        return [f(p) for f, p in zip(transforms, point)]
+
+class CSV:
+    def __init__(self, content, delim, transforms=[]):
+        if type(content) == str:
+            content = open(content).readlines()
+        self.headers = list(map(lambda header: header.strip(), content[0].split(delim)))
+        self.data = list(map(lambda line: transform(line.split(delim), transforms), content[1:]))
+    
+    def get(self, *cols):
+        indices = []
+        for search in cols:
+            for i, col in enumerate(self.headers):
+                if search == col:
+                    indices += [i]
+                    break
+            else:
+                raise Exception(f"Column not found '{search}', available columns are: " + str(self.headers))
+        for point in self.data:
+            yield [point[i] for i in indices]
+    
+    def get_simple(self, col):
+        for point in self.get(col):
+            yield point[0]
