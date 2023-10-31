3b3bee0119193ca52bd727027eda9f47322b5af1
Bruno Curzi-Laliberte <bruno.bcl@sympatico.ca>
2023-10-31 17:09:50 -0400

    finished reshaping parameter data for simulation

--- src/simulation/covid-scenario/covid_scenario.py
+++ src/simulation/covid-scenario/covid_scenario.py
@@ -11,8 +11,12 @@ def can_gov_ages_to_range(ages):
         ages = ages.split('years')[0].split(' to ')
         return [int(ages[0]), int(ages[1])]
 
-contact_last_age_delim = 70
-pop_last_age_delim = 100
+def polymod_age_to_range(ages):
+    if '+' in ages:
+        return [int(ages.split('+')[0]), 0]
+    else:
+        ages = ages.split('-')
+        return [int(ages[0]), int(ages[1])]
 
 # https://www150.statcan.gc.ca/n1/en/pub/91-215-x/91-215-x2019001-eng.pdf?st=QscDWW2B
 pop_csv = CSV('pop-2019.csv', ',', [can_gov_ages_to_range, int])
@@ -21,10 +25,12 @@ pop_csv = CSV('pop-2019.csv', ',', [can_gov_ages_to_range, int])
 comorbidity_csv = CSV('cchs-out.csv', ',', [can_gov_ages_to_range, float]) 
 
 # https://github.com/epiforecasts/socialmixr/issues/1
-contact_csv = CSV('polymod_matrix.csv', ',', [float] * (int(contact_last_age_delim / 5) + 1)) 
+contact_csv = CSV('polymod_matrix.csv', ',', [float] * 15)
+contact_csv.headers=list(map(polymod_age_to_range, contact_csv.headers))
 
 # now we need to reshape the data into the same groups used by the researchers
-# The last age group (i = 16) included the population aged 75 and older
+# "The population was divided into 5-year age groups using 2019 population estimates for Ontario
+#  (Statistics Canada 2019). The last age group (i = 16) included the population aged 75 and older."
 
 # first lets do the contacts matrix
 # we need to:
@@ -55,12 +61,28 @@ p_75_80 = list(pop_csv.get_simple('count'))[-6]
 p_comorbidity_given_75_plus = (p_comorbidity_given_80_plus * p_80_plus + p_comorbidity_given_75_80 * p_75_80)/(p_75_80 + p_80_plus)
 # change 75-79 to 75+, set new value, remove 80+
 comorbidity_csv.data = comorbidity_csv.data[:-2] + [[[75, 0], p_comorbidity_given_75_plus]]
+# and then we need to merge 15-17 and 18-19, we will average since we don't have fine grained population data
+comorbidity_csv.data = comorbidity_csv.data[:1] +\
+    [[[15, 19], (comorbidity_csv.data[1][1] + comorbidity_csv.data[2][1]) / 2]] +\
+    comorbidity_csv.data[3:]
+# then we need to use data from another source:
+# "For younger age groups (< 12 yr), we used estimates from Moran and colleagues"
+# they produced: Prevalence of chronic conditions (2–11 years): Present(weighted) = 10.7%
+# we will use this value for ages 0-4, 5-9 and average it with our current data at 12-14 for 10-14
+comorbidity_csv.data = [[[0, 4], 0.107], [[5, 9], 0.107],
+    [[10, 14], 0.107 * 0.4 + comorbidity_csv.data[0][1] * 0.6]
+    ] + comorbidity_csv.data[3:]
+
 
 # finally we will do the population table
 count_75_plus = sum(list(pop_csv.get_simple('count'))[-6:])
 pop_csv.data = pop_csv.data[:-6]
 pop_csv.data += [[[75, 0], count_75_plus]]
 
+
+print(contact_csv)
+print(comorbidity_csv)
+print(pop_csv)
 
 def main():
     project_name = 'covid'
