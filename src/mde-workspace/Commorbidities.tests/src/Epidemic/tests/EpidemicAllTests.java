9805b7613e4c63d848078dde95b2bda5f8cfaccb
SarinaHamedani <sarinahamedani3@gmail.com>
2023-11-15 18:42:38 -0500

    Add new workspace for and metamodel for MDE

---
+++ src/mde-workspace/Commorbidities.tests/src/Epidemic/tests/EpidemicAllTests.java
@@ -0,0 +1,46 @@
+/**
+ */
+package Epidemic.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>Epidemic</b></em>' model.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class EpidemicAllTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new EpidemicAllTests("Epidemic Tests");
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EpidemicAllTests(String name) {
+		super(name);
+	}
+
+} //EpidemicAllTests
